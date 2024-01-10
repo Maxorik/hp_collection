e9133a9eb14d24b4f743b051ba2709eada30a36f
Your Name <gms441@mail.ru>
2024-01-10 22:32:03 +0700

    Добавлен локальный стор

--- src/store/store.js
+++ src/store/store.js
@@ -10,7 +10,11 @@ class Collection {
 
     /** получаем коллекцию */
     getCatalog() {
+        const localCollection = localStorage.getItem('hp_collection') ? JSON.parse(localStorage.getItem('hp_collection')) : [];
         this.collectionList = catalog.filter(item => item.code.match(/hp\d+/) && +item.year > 2017);
+        this.collectionList.forEach((item, index) => {
+            item.checked = item.code === localCollection[index] && localCollection[index].code ? localCollection[index].checked : item.checked;
+        })
     }
 
     /** Состояние коллекции */
@@ -20,9 +24,16 @@ class Collection {
 
     /** отметить найденный экземпляр */
     setCheck(id, state) {
-        // TODO api
-
         this.collectionList = this.collectionList.map(item => item.code === id ? { ...item, checked: state.toString() } : item );
+        this.updateLocalStorage();
+    }
+
+    /** Обновить локальный стор */
+    updateLocalStorage() {
+        const collection = this.collectionList.map(item => {
+            return { code: item.code, checked: item.checked }
+        } );
+        localStorage.setItem('hp_collection', JSON.stringify(collection));
     }
 }
 
