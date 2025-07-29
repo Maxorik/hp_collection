f1c0e5ad4dcd4f889d046c3da368434a5ecc53c6
Maxorik <gfifful@gmail.com>
2025-07-29 20:41:07 +0700

    remove bg from images and compress

---
+++ utils/scripts/removebg.py
@@ -0,0 +1,25 @@
+# pip install rembg
+
+from rembg import remove
+from PIL import Image
+import os
+
+input_folder = "../../public/catalog_images"
+output_folder = "no-background"
+os.makedirs(output_folder, exist_ok=True)
+
+for filename in os.listdir(input_folder):
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.webp')):
+        input_path = os.path.join(input_folder, filename)
+        output_path = os.path.join(output_folder, filename)
+
+        with open(input_path, "rb") as input_file:
+            input_data = input_file.read()
+            output_data = remove(input_data)
+
+        with open(output_path, "wb") as output_file:
+            output_file.write(output_data)
+
+        print(f'Обработан файл {input_path}...')
+
+print("Все изображения обработаны")
