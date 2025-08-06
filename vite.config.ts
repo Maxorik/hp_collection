778fbbd9907f97e339876775364b3c752e86a0aa
Maxorik <gfifful@gmail.com>
2025-08-06 09:53:14 +0700

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+            },
+            devOptions: {
+                enabled: true
+            }
+        })
+    ],
     base: './',
     server: {
         open: true,
